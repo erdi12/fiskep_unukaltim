6a8d428d7f84abfd993833582ae998984c357310
erdi12 <erdinurikhsan0601@gmail.com>
2024-01-25 17:18:32 +0800

    memperbaiki forgot password

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -23,7 +25,25 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
+        // $this->registerPolicies();
+        ResetPassword::toMailUsing(function ($notifiable, $token)
+        {
+            $url = url(route('password.reset', [
+                'token' => $token,
+                'email' => $notifiable->email,
+            ]));
+
+            $subject = 'Reset Password untuk ' . $notifiable->email;
+
+            return (new MailMessage)
+                    ->from('akademik@unukaltim.ac.id', 'Tim IT UNU Kaltim')
+                    ->subject($subject)
+                    ->greeting("Assalamu'alaikum, Wr.Wb.")
+                    ->line('Kami mendeteksi bahwa Anda mengalami kesulitan dalam mengklik tombol "Login". Jika itu terjadi, silahkan klik tombol "Klik Disini Untuk Merubah Password" dibawah ini:')
+                    ->action('Klik Disini Untuk Merubah Password', $url)
+                    ->line('Terima Kasih,')
+                    ->salutation('Tim IT UNU Kaltim');          
+        });
 
         //
     }
--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'akademik@unukaltim.ac.id'),
+        'name' => env('MAIL_FROM_NAME', 'Hello UNU KALTIM'),
     ],
 
     /*
